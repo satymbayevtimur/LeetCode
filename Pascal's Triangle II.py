2330ec88e3a35c2aab3cd2b61427792dafe08cc6
satymbayevtimur <satymbayevtimur05@gmail.com>
2024-06-01 19:03:26 +0500

    Create Pascal's Triangle II.py

---
+++ Pascal's Triangle II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        curr, ans = 1, [1]
+
+        for i in range(1,rowIndex+1):
+            curr = curr*(rowIndex+1-i)//i
+
+            ans.append(curr) 
+
+        return ans
